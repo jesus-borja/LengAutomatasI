1d7f174a88704f522bf130ca4825d5942a7a876f
Jesus Borja <yisus.bh25@gmail.com>
2024-04-23 23:48:07 -0600

    Add exercise B solution for 3.2

--- Tema 3/Tarea 3.1/ejercicios3.2.py
+++ Tema 3/Tarea 3.1/ejercicios3.2.py
@@ -21,3 +21,18 @@ if __name__ == "__main__":
     }
     afd = AFD.AFD(Q, sigma, delta, q0, F)
     AFD.write_dot_diagram_from_DFA(afd, filename="A", create_image=True)
+
+    # B) El lenguaje de las cadenas no vacias donde toda a está entre dos bs
+    Q = {'q0', 'q1', 'q2',}
+    q0 = 'q0'
+    F = {'q1'}
+    delta = {
+        ('q0', 'a'): 'q2',
+        ('q0', 'b'): 'q1',
+        ('q1', 'a'): 'q0',
+        ('q1', 'b'): 'q1',
+        ('q2', 'a'): 'q2',
+        ('q2', 'b'): 'q2',
+    }
+    afd = AFD.AFD(Q, sigma, delta, q0, F)
+    AFD.write_dot_diagram_from_DFA(afd, filename="B", create_image=True)
