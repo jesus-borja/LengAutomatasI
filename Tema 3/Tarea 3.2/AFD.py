c28a22d4ace920b083b1296c5f02e501743ace78
Jesus Borja <yisus.bh25@gmail.com>
2024-05-02 23:37:52 -0600

    Entrega Tarea 3.2

---
+++ Tema 3/Tarea 3.2/AFD.py
@@ -0,0 +1,77 @@
+import subprocess
+
+class AFD:
+    def __init__(self, Q: set, sigma: set, delta: dict, q0: str, F: set):
+        self.Q = Q         # estados
+        self.sigma = sigma # alfabeto
+        self.delta = delta # transiciones
+        self.q0 = q0       # estado inicial
+        self.F = F         # estados finales
+    
+    def evaluar_cadena(self, cadena: str) -> bool:
+        estado_actual = self.q0
+        for simbolo in cadena:
+            estado_actual = self.delta.get((estado_actual, simbolo))
+            
+        print(f"{cadena}: {estado_actual in self.F}")
+        return estado_actual in self.F
+
+def write_dot_diagram_from_DFA(afd: AFD, filename="AFD", create_image=False) -> None:
+    with open(f"{filename}.dot", "w") as f:
+        print("/* transition diagram for DFA */", file=f)
+        print("/* created by github.com/jesus-borja */", file=f)
+        print("", file=f)
+        print("digraph {", file=f)
+        print("  rankdir = LR", file=f)
+        print("  bgcolor = \"#0d1117\"", file=f)
+        print("  node [shape = circle, color = \"#ffdd33\", fontcolor=\"#f4f4ff\"]", file=f)
+        print("  edge [color = \"#ffdd33\", fontcolor=\"#f4f4ff\"]", file=f)
+        print("", file=f)
+        print("  \"Start\" [label = \"\", shape = none]", file=f)
+        print("", file=f)
+
+        # escribe todos los estados
+        for q in afd.Q:
+            if q in afd.F:
+                print(f"  \"{q}\" [shape=doublecircle]", file=f)
+            else:
+                print(f"  \"{q}\" [shape=circle]", file=f)
+
+        # escribe todas las aristas
+        print(f"  \"Start\" -> \"{afd.q0}\"", file=f)
+        for key, value in afd.delta.items():
+            print(f"  \"{key[0]}\" -> \"{value}\" [label = \"{key[1]}\"]", file=f)
+        
+        print("}", file=f)
+
+    if create_image: 
+        create_png_diagram(filename, filename)
+
+def create_png_diagram(dot_file: str, output_name: str):
+    cmd = f"dot {dot_file}.dot -Tpng -o {output_name}.png"
+    print(f"Creando imagen {output_name}.png con el archivo {dot_file}.dot")
+    print(f"$ {cmd}")
+
+    return_code = subprocess.run(cmd)
+    if return_code.returncode == 0:
+        print(f"Imagen \"{output_name}.png\" creada correctamente")
+    else:
+        print(f"No se pudo crear la imagen {output_name}.png")
+
+if __name__ == "__main__":
+
+    # ejemplo
+    Q = {'q0', 'q1'}
+    sigma = {'0', '1'}
+    delta = {
+       ('q0', '0'): 'q0',
+       ('q0', '1'): 'q1',
+       ('q1', '0'): 'q1',
+       ('q1', '1'): 'q0',
+    }
+    q0 = 'q0'
+    F = {'q0'}
+    
+    afd = AFD(Q, sigma, delta,q0, F)
+    write_dot_diagram_from_DFA(afd, filename="ejemplo", create_image=True)
+    
---
+++ Tema 3/Tarea 3.2/Tarea 3.2.md
@@ -0,0 +1,68 @@
+# Caso real de un autómata
+
+Dentro de los ejemplos más communes para los autómatas finitos son las máquinas expendedoras, cajeros, switches, etc., pero uno con el que probablemente interactuamos seguidamente es con la estación del TuzoBus, la cual cuenta con un torniquete de acceso que funciona ya sea con la tarjeta del servicio o con monedas.
+
+Para ejemplificar el funcionamiento de este autómata nos centraremos en la función con monedas.
+
+Haciendo un análisis sobre el mismo vemos que tiene dos estados principales: "Cerrado" y "Abierto", pudiendo entre cualquiera de estos estados aceptar una moneda de 1, 2, 5 o 10 pesos.
+
+Cada que insertamos una moneda el autómata cambia su estado para representar la cantidad de dinero que se ha insertado.
+
+Una vez se alcance la cantidad de 10 pesos el autómata permite la entrada a la estación.
+
+El alfabeto del autómata entonces son los valores de las monedas, los estados es la cantidad de dinero que se ha insertado:
+
+$\Sigma = \{1, 2, 5, 10\}$
+
+$Q = \{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10\}$
+
+Las transiciones se dan por cada moneda que podemos introducir al sistema para llegar a la suma de 10:
+
+$$
+\begin{align}
+\delta &= \{ \\
+&(0, 1) = 1, \\
+&(1, 1) = 2, \\
+&(2, 1) = 3, \\
+&(3, 1) = 4, \\
+&(4, 1) = 5, \\
+&(5, 1) = 6, \\
+&(6, 1) = 7, \\
+&(7, 1) = 8, \\
+&(8, 1) = 9, \\
+&(9, 1) = 10, \\
+&(0, 2) = 2, \\
+&(2, 2) = 4, \\
+&(4, 2) = 6, \\
+&(6, 2) = 8, \\
+&(8, 2) = 10, \\
+&(1, 2) = 3, \\
+&(3, 2) = 5, \\
+&(5, 2) = 7, \\
+&(7, 2) = 9, \\
+&(0, 5) = 5, \\
+&(5, 5) = 10, \\
+&(1, 5) = 6, \\
+&(2, 5) = 7, \\
+&(3, 5) = 8, \\
+&(4, 5) = 9, \\
+&(0, 10) = 10 \\
+&\}
+\end{align}
+$$
+
+Por último, el estado inicial es siempre empezar en 0, y el estado final es cuando se llega a la suma de 10:
+
+$q_0 = 0$
+
+$F = \{10\}$
+
+El grafo que representa este autómata es el siguiente:
+
+![Diagrama de transiciones](img/tuzo.png)
+
+Para este diseño decidimos omitir las transiciones que sobrepasan a la suma de 10 por simplicidad, además de que en la implementación actual, cualquier valor mayor de 10 sigue siendo un estado de aceptación.
+
+# Conclusión
+
+Los autómatas finitos tienen muchas aplicaciones dentro y fuera de las ciencias computacionales y nos ayudan a simplificar las soluciones de problemas que de otra manera resultarían muy complejos de resolver, como puede ser este mismo ejemplo, en el que tendríamos que tener varias condiciones para saber cuál es el siguiente estado a partir de la moneda que se introdujo.
